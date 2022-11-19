6421a4e41ae0070ccc6aa49fc9af299707514802
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-11-20 10:13:07 +1100

    Add basic elements in index.js for contents

---
+++ dist/main.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utility__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utility */ \"./src/utility.js\");\n// Add a function to open each tab and wipe out\n// the current contents and run the correct tab module.\n\n// div #content\n\n//  div #content have same padding x and y axis.\n//  Create buttons for each tabs.\n\n// div #title\n// tab title heading and the border bottom should be the same.\n\n// div # content-body\n\n\nconst contentContainer = document.getElementById(\"content\");\n\nconst historyTabButton = (0,_utility__WEBPACK_IMPORTED_MODULE_0__.createElement)(\"button\", \"History\");\nconst menuTabButton = (0,_utility__WEBPACK_IMPORTED_MODULE_0__.createElement)(\"button\", \"Menu\");\nconst contactTabButton = (0,_utility__WEBPACK_IMPORTED_MODULE_0__.createElement)(\"button\", \"Contact\");\ncontentContainer.append(historyTabButton, menuTabButton, contactTabButton);\n\nconst contentTitle = (0,_utility__WEBPACK_IMPORTED_MODULE_0__.createElement)(\"div\", \"Content title is here\");\ncontentTitle.id = \"content-title\";\n\nconst contentBodyContainer = (0,_utility__WEBPACK_IMPORTED_MODULE_0__.createElement)(\"div\", \"Content body is here\");\ncontentBodyContainer.id = \"content-body\";\ncontentContainer.append(contentTitle, contentBodyContainer);\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/utility.js":
+/*!************************!*\
+  !*** ./src/utility.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createElement\": () => (/* binding */ createElement)\n/* harmony export */ });\nfunction createElement(elementType, elementText = \"\") {\n  const element = document.createElement(elementType);\n  element.innerText = elementText;\n  return element;\n}\n\n\n//# sourceURL=webpack://restaurant-page/./src/utility.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@
 //  div #content have same padding x and y axis.
 //  Create buttons for each tabs.
 
-// div # content-body
-
 // div #title
 // tab title heading and the border bottom should be the same.
 
+// div # content-body
+import { createElement } from "./utility";
 
+const contentContainer = document.getElementById("content");
+
+const historyTabButton = createElement("button", "History");
+const menuTabButton = createElement("button", "Menu");
+const contactTabButton = createElement("button", "Contact");
+contentContainer.append(historyTabButton, menuTabButton, contactTabButton);
+
+const contentTitle = createElement("div", "Content title is here");
+contentTitle.id = "content-title";
+
+const contentBodyContainer = createElement("div", "Content body is here");
+contentBodyContainer.id = "content-body";
+contentContainer.append(contentTitle, contentBodyContainer);
