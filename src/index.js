0a3579490e274ca2317caa9dcd02b4f4ba4d7106
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-11-20 09:29:31 +1100

    Add webpack again and basic setup for index.html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 // Add a function to open each tab and wipe out
 // the current contents and run the correct tab module.
 
-console.log("It is working");
+// div #content
+
+//  div #content have same padding x and y axis.
+//  Create buttons for each tabs.
+
+// div # content-body
+
+// div #title
+// tab title heading and the border bottom should be the same.
+
+
