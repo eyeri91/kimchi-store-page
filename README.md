90754b71c7fd3931075da21bb316d07df4692123
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-11-30 18:15:12 +0400

    Implement basic history page and add an image to the tab

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # restaurant-page
+
+<!-- Images for kimchi
+
+1. history tab -> https://pixabay.com/images/id-7613312/ by lpegasu
+ -->
---
+++ src/tab_modules/history.js
@@ -0,0 +1,28 @@
+import { createElement } from "../utils/utility";
+import kimchiHistoryImg from "../imgs/kimchi-history.jpg";
+function openHistoryPage() {
+  const contentTitle = document.getElementById("content-title");
+  contentTitle.textContent = "History";
+
+  const contentBodyContainer = document.getElementById("content-body");
+  contentBodyContainer.classList.add(
+    "flex",
+    "flex-col",
+    "justify-center",
+    "items-center"
+  );
+  const kimchiImageForHistoryTab = new Image();
+  kimchiImageForHistoryTab.src = kimchiHistoryImg;
+  kimchiImageForHistoryTab.setAttribute("width", 400);
+  kimchiImageForHistoryTab.classList.add("rounded-sm", "my-2");
+  contentBodyContainer.append(kimchiImageForHistoryTab);
+
+  const storeHistoryParagraph = createElement(
+    "p",
+    "The history has started since 1991. The best kimchi in the town. That was the aim of the ...."
+  );
+  storeHistoryParagraph.classList.add("my-3", "px-2", "max-w-fit");
+  contentBodyContainer.append(storeHistoryParagraph);
+}
+
+export default openHistoryPage;
