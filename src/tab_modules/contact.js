0108d3e2eb99adb7ceae614a87cde4ed66abd9de
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-12-05 23:36:27 +0400

    Add branch details container below each image

--- src/tab_modules/contact.js
+++ src/tab_modules/contact.js
@@ -1,5 +1,5 @@
 import {
-  createElemet,
+  createElement,
   makeSemiTitle,
   getImageSourceAndStyle,
 } from "../utils/utility";
@@ -10,16 +10,25 @@ import budapestImage from "../imgs/budapest.jpg";
 
 const branchObjectsList = [
   {
-    title: "Dusbai",
+    title: "Dubai",
     image: dubaiImage,
+    location: "Blah",
+    phone: "number",
+    email: "dubai_kimchi@mkbty.com",
   },
   {
     title: "Seoul",
     image: seoulImage,
+    location: "Blah",
+    phone: "number",
+    email: "seoul_kimchi@mkbty.com",
   },
   {
     title: "Budapest",
     image: budapestImage,
+    location: "Blah",
+    phone: "number",
+    email: "budapest_kimchi@mkbty.com",
   },
 ];
 
@@ -40,6 +49,25 @@ function openContactPage() {
     contentBodyContainer.append(branchDiv);
     const branchImage = getImageSourceAndStyle(branch.image, 400);
     contentBodyContainer.append(branchImage);
+
+    const branchDetailsDiv = createElement("div", "");
+    contentBodyContainer.append(branchDetailsDiv);
+    branchDetailsDiv.classList.add(
+      "text-left",
+      "p-2",
+      "pt-1",
+      "rounded-md",
+      "bg-gray",
+      "w-[400px]"
+    );
+    const branchDetailsHeading = createElement("h", branch.title);
+    branchDetailsHeading.classList.add("text-xl", "pb-2");
+    branchDetailsDiv.append(branchDetailsHeading);
+    const branchDetailsParagrapgh = createElement(
+      "p",
+      `Address: ${branch.location} \n Phone: ${branch.phone} \n Email: ${branch.email}`
+    );
+    branchDetailsDiv.append(branchDetailsParagrapgh);
   }
 }
 
